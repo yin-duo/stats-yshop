e32bd70c49f04e556906295b09043afe6545ac10
jinjiajin <875358601@qq.com>
2021-08-21 14:42:19 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@
   h.href = 'https://yshopimg.oss-cn-shanghai.aliyuncs.com/css/menu.css';
   h.setAttribute('rel','stylesheet');
   h.setAttribute('type','text/css');
+
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(h, s);
 })();
 
 export function getSystemInfo() {
+  console.log('当前站点 ' + document.domain + ' 使用 Yshop商城后台管理系统 框架技术')
+  console.log('上海银舵网络科技有限公司版权所有 www.yin-duo.com')
+  console.log('Yshop - StatsCS')
 }
